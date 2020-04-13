85bf5aeb53c74755207db9e1db34e1f15704d0a8
Rado-K <heheheheto@gmail.com>
2020-04-13 15:53:36 +0300

    Update component view

---
+++ src/Web/SFMBE.Client/Pages/Character/CharacterInfo.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace SFMBE.Client.Pages.Character
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Threading.Tasks;
+
+  public partial class CharacterInfo
+  {
+
+  }
+}
